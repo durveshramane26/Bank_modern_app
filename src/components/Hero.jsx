af486caacb12bd893e75d19eb93661b1c6b79e5c
durveshramane26 <durveshramane6@gmail.com>
2022-08-15 23:32:57 +0530

    Added color gradient

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { discount } from '../assets';
+import { discount, robot } from '../assets';
 import styles from "../style";
 import GetStarted from './GetStarted';
 
@@ -22,13 +22,22 @@ const Hero = () => {
                     The Next <br className='sm:block hidden' /> {" "}
                     <span
                     className='text-gradient'>Generation</span> {" "}
-                    Payment Method.
                 </h1>
                 <div className='ss:flex hidden md:mr-4 mr-0'>
                     <GetStarted />
                 </div>
-
             </div>
+
+            <h1 className='font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100px] leading-[75px] w-full'>Payment Method</h1> 
+            <p className={`${styles.paragraph} max-w-[470] mt-5`}>Our team of experts uses a methodology to identify the credit cards most likely to fit your needs. We examine annual percentage rates,
+            annual fees.</p>
+        </div>
+
+        <div> 
+            <img src={robot} alt='billing' className='w-[100%] h-[100%] relative z-[5]'  />
+            <div className='absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient'/>
+            <div className='absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient'/>
+            <div className='absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient'/>
         </div>
      </section>
   )
