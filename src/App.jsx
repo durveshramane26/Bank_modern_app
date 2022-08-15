54bdc6904a38a96875eed026477598d088918bac
durveshramane26 <durveshramane6@gmail.com>
2022-08-15 20:16:16 +0530

    Header styled

--- src/App.jsx
+++ src/App.jsx
@@ -19,14 +19,7 @@ const App = () => (
     
     <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
-        Stats 
-        Business 
-        Billing 
-        CardDeal 
-        Testimonials
-        Clients 
-        CTA 
-        Footer 
+        
       </div>
     </div>
   </div>
