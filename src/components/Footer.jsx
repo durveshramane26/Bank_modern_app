d358af8474acf30375887306017d1f1fb426f20a
durveshramane26 <durveshramane6@gmail.com>
2022-08-17 00:47:49 +0530

    Added Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -36,7 +36,12 @@ const Footer = () => (
                 ))}
             </div>
         </div>
-        
+
+        <div className='w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]'>
+            <p>
+                Copyright Ⓒ 2022 HooBank. All Rights Reserved.
+            </p>
+        </div>
     </section>
 )
 export default Footer
